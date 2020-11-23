916002b2556c154a8dc9870b8432c5a04fefd114
hungle <hungle2048@gmail.com>
2020-11-23 15:06:26 +0700

    add some ajax feature

--- app.rb
+++ app.rb
@@ -1,16 +1,38 @@
 require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
 
 set :server, :thin
 
 get '/' do
-  send_file "html/index.html"
+  send_file "views/index.html"
 end
 
-get '/bottles', provides: 'text/event-stream' do
-  stream do |out|
-  	100.times do |i|
-  	  out << "#{i} bottle(s) on a wall...\n"
-      sleep 0.5
-  	end
+class Stream
+  def each
+    100.times { |i| yield "#{i}\n" }
   end
+end
+
+get('/bottles') { Stream.new }
+
+
+get '/fetch' do
+
+  doc = Nokogiri::HTML(open('https://github.com/'))
+  response = {
+    :content => doc.content,
+    :some_other_shit => 'poop',
+  }
+
+  response.to_json
+end
+
+get '/json' do
+  erb :json
+end
+
+get '/jsonp' do
+  erb :jsonp
 end
