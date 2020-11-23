26b70af47289c23884f37982d7abc28e970d7d87
hungle <hungle2048@gmail.com>
2020-11-23 15:48:45 +0700

    add test

--- Gemfile
+++ Gemfile
@@ -4,3 +4,8 @@ ruby '2.6.5'
 
 gem "puma"
 gem "sinatra"
+
+group :test do
+  gem "rspec"
+  gem "rack-test"
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+require "./app.rb"
+
+RSpec.describe 'The HelloWorld App' do
+  #include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/'
+    expect(last_response).to be_ok
+    #expect(last_response.body).to eq('Hello World')
+  end
+end
